8d391ebf659141899284dd3f3f3362c5889cd255
Drew Parker <drew.i.parker@gmail.com>
2017-01-25 20:34:43 -0500

    Moving towards flexbox for layout. Implemented in Navbar. F bootstrap

--- src/Header.js
+++ src/Header.js
@@ -8,15 +8,23 @@ class Header extends Component {
     render() {
         return(
             <div className='header'>
-                <ul>
-                    <li className="header-list-item"><Link to="/about-me">About Me</Link></li>
-                    <li className="header-list-item"><Link to="/experience">Experience</Link></li>
-                    <li className="header-list-item"><Link to="/projects">Projects</Link></li>
-                    <li className="header-list-item"><Link to="/contact">Contact</Link></li>
-                </ul>
+                <div className="flex-menu-container">
+                    <div className="flex-menu-item"><Link to="/about-me">About Me</Link></div>
+                    <div className="flex-menu-item"><Link to="/experience">Experience</Link></div>
+                    <div className="flex-menu-item"><Link to="/projects">Projects</Link></div>
+                    <div className="flex-menu-item"><Link to="/contact">Contact</Link></div>
+                </div>
+
             </div>
         )
     }
 }
 
 export default Header
+
+// <ul>
+//     <li className="header-list-item"><Link to="/about-me">About Me</Link></li>
+//     <li className="header-list-item"><Link to="/experience">Experience</Link></li>
+//     <li className="header-list-item"><Link to="/projects">Projects</Link></li>
+//     <li className="header-list-item"><Link to="/contact">Contact</Link></li>
+// </ul>
