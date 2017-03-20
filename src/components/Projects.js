054be4ea01ea642ea11759f3b3c9bc3cf76cf5a0
Drew Parker <drew.i.parker@gmail.com>
2017-03-20 19:00:56 -0400

    Added grid layout and placeholder imgs to projects section.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,24 +13,57 @@ class Projects extends Component {
                     <div className="container item-wrapper">
                         <div className="row">
                             <div id="movie" className="project-item">
-                                <div className="hidden-xs project-img">
+                                <div className="project-title">Movies</div>
+                                <hr></hr>
+                                <div className="hidden-xs col-sm-6 project-img">
                                     <img
                                         src={require("../images/movie_app_2.png")}
                                         role={"presentation"} />
                                 </div>
-                                <div className="hidden-sm hidden-md hidden-lg project-img-sm">
+                                <div className="hidden-sm hidden-md hidden-lg col-xs-12 project-img-sm">
                                     <img
                                         src={require("../images/movie_app_3.png")}
                                         role={"presentation"} />
                                 </div>
+                                <div className="col-xs-12 col-sm-6 project-content-wrapper">
+                                    <div className="project-content"></div>
+                                </div>
                             </div>
 
                             <div id="ebay" className="project-item">
-
+                                <div className="project-title">Ebay Clone</div>
+                                <hr></hr>
+                                <div className="hidden-xs col-sm-6 project-img">
+                                    <img
+                                        src={require("../images/movie_app_2.png")}
+                                        role={"presentation"} />
+                                </div>
+                                <div className="hidden-sm hidden-md hidden-lg col-xs-12 project-img-sm">
+                                    <img
+                                        src={require("../images/movie_app_3.png")}
+                                        role={"presentation"} />
+                                </div>
+                                <div className="col-xs-12 col-sm-6 project-content-wrapper">
+                                    <div className="project-content"></div>
+                                </div>
                             </div>
 
                             <div id="geo" className="project-item">
-
+                                <div className="project-title">GeoQuiz</div>
+                                <hr></hr>
+                                <div className="hidden-xs col-sm-6 project-img">
+                                    <img
+                                        src={require("../images/movie_app_2.png")}
+                                        role={"presentation"} />
+                                </div>
+                                <div className="hidden-sm hidden-md hidden-lg col-xs-12 project-img-sm">
+                                    <img
+                                        src={require("../images/movie_app_3.png")}
+                                        role={"presentation"} />
+                                </div>
+                                <div className="col-xs-12 col-sm-6 project-content-wrapper">
+                                    <div className="project-content"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
