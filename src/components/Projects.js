bf889c92e1c2e1e221d74d44ac4b1bf390ac8115
Drew Parker <drew.i.parker@gmail.com>
2017-03-28 17:54:22 -0400

    Completed the content for the proj section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,7 +30,8 @@ class Projects extends Component {
                                 <div className="col-xs-12 col-sm-6 project-content-wrapper">
                                     <div className="project-content">
                                         <span>
-                                            Trailers is an app that lets movie lover
+                                            &nbsp;
+                                            Trailers is an app that lets movie lovers
                                             browse now playing and upcoming movies
                                             and quickly view their trailers. The
                                             trailers are presented in a modal, so it's
@@ -40,6 +41,7 @@ class Projects extends Component {
                                         <br></br>
                                         <br></br>
                                         <span>
+                                            &nbsp;
                                             Movies are displayed on handsome
                                             cards featuring the poster, director,
                                             runtime, and the mpaa rating. The
@@ -51,10 +53,10 @@ class Projects extends Component {
                                             <div className="tech-card"><span>React</span></div>
                                             <div className="tech-card"><span>Redux</span></div>
                                             <div className="tech-card"><span>AJAX</span></div>
-                                            <div className="tech-card"><span>APIs</span></div>
+                                            <div className="tech-card"><span>RESTful APIs</span></div>
                                             <div className="tech-card"><span>jQuery</span></div>
                                             <div className="tech-card"><span>Bootstrap</span></div>
-                                            <div className="tech-card"><span>Router</span></div>
+                                            <div className="tech-card"><span>React-Router</span></div>
                                             <div className="tech-card"><span>ES6</span></div>
                                         </div>
                                     </div>
@@ -63,7 +65,7 @@ class Projects extends Component {
                         </div>
                         <div className="row">
                             <div id="ebay" className="project-item">
-                                <div className="project-title">Ebay Clone</div>
+                                <div className="project-title">Auctioneer</div>
                                 <hr></hr>
                                 <div className="hidden-xs col-sm-6 project-img">
                                     <img
@@ -80,32 +82,37 @@ class Projects extends Component {
                                 <div className="col-xs-12 col-sm-6 project-content-wrapper">
                                     <div className="project-content">
                                         <span>
-                                            Trailers is an app that lets movie lover
-                                            browse now playing and upcoming movies
-                                            and quickly view their trailers. The
-                                            trailers are presented in a modal, so it's
-                                            quite easy to start one, dismiss it, and
-                                            move on to the next.
+                                            &nbsp;
+                                            Auctioneer is an ecommerce platform
+                                            that people can use to sell and buy
+                                            used goods. Similar to Ebay, Auctioneer
+                                            allows users to bid anytime while
+                                            logged in. The highest bidder wins!
                                         </span>
                                         <br></br>
                                         <br></br>
                                         <span>
-                                            Movies are displayed on handsome
-                                            cards featuring the poster, director,
-                                            runtime, and the mpaa rating. The
-                                            Movie Database provides the API.
+                                            &nbsp;
+                                            Auction items are displayed on nicely
+                                            styled cards. Each item has a photo,
+                                            a description, its current bid price, a description,
+                                            and an expiration date. Items can also
+                                            be filtered by category.
                                         </span>
                                         <hr></hr>
                                         <div id="tech-title"><span>Tech used</span></div>
                                         <div className="project-tech">
                                             <div className="tech-card"><span>React</span></div>
                                             <div className="tech-card"><span>Redux</span></div>
-                                            <div className="tech-card"><span>AJAX</span></div>
-                                            <div className="tech-card"><span>APIs</span></div>
+                                            <div className="tech-card"><span>Express.js</span></div>
+                                            <div className="tech-card"><span>Node.js</span></div>
+                                            <div className="tech-card"><span>MySql</span></div>
                                             <div className="tech-card"><span>jQuery</span></div>
                                             <div className="tech-card"><span>Bootstrap</span></div>
-                                            <div className="tech-card"><span>Router</span></div>
                                             <div className="tech-card"><span>ES6</span></div>
+                                            <div className="tech-card"><span>React-Router</span></div>
+                                            <div className="tech-card"><span>RESTful API</span></div>
+                                            <div className="tech-card"><span>localstorage</span></div>
                                         </div>
                                     </div>
                                 </div>
@@ -131,6 +138,7 @@ class Projects extends Component {
                                 <div className="col-xs-12 col-sm-6 project-content-wrapper">
                                     <div className="project-content">
                                         <span>
+                                            &nbsp;
                                             AtlantaVibes is a full-stack application
                                             that aggregates data from the top music
                                             festivals, in Atlanta, into one easy to
@@ -143,12 +151,15 @@ class Projects extends Component {
                                         <div className="project-tech">
                                             <div className="tech-card"><span>React</span></div>
                                             <div className="tech-card"><span>Redux</span></div>
-                                            <div className="tech-card"><span>AJAX</span></div>
-                                            <div className="tech-card"><span>APIs</span></div>
+                                            <div className="tech-card"><span>Express.js</span></div>
+                                            <div className="tech-card"><span>Node.js</span></div>
+                                            <div className="tech-card"><span>MySql</span></div>
                                             <div className="tech-card"><span>jQuery</span></div>
                                             <div className="tech-card"><span>Bootstrap</span></div>
-                                            <div className="tech-card"><span>Router</span></div>
                                             <div className="tech-card"><span>ES6</span></div>
+                                            <div className="tech-card"><span>NPM</span></div>
+                                            <div className="tech-card"><span>Sass</span></div>
+                                            <div className="tech-card"><span>React-Router</span></div>
                                         </div>
                                     </div>
                                 </div>
