4a3a357c583cfcf242268cbf23118b3e72bbdda0
Drew Parker <drew.i.parker@gmail.com>
2017-04-03 14:24:45 -0400

    AtlantaVibes can now be accessed when you click on img in projects section.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,67 +11,65 @@ class Projects extends Component {
                 <div style={{backgroundColor: `#ABC8AF`, width: `100%`, height: `5px`}}></div>
                 <div className="container-fluid projects-container">
                     <div className="container item-wrapper">
+
                         <div className="row">
-                            <div id="movie" className="project-item">
+                            <div id="geo" className="project-item">
                                 <div className="project-header">
-                                    <div className="project-title"><span>Trailers</span>
-                                        <div className="project-sub-title">An app to view movie trailers</div>
+                                    <div className="project-title"><span>AtlantaVibes</span>
+                                        <div className="project-sub-title">Get info on Atlanta's music festivals</div>
                                     </div>
                                     <div className="github-repo">
-                                        <a target="_blank" href="https://github.com/drewcoparker/movie-app-reactified">
+                                        <a target="_blank" href="https://github.com/drewcoparker/AtlantaVibes">
                                             <div><span>GitHub</span></div>
                                         </a>
                                     </div>
                                 </div>
                                 <hr></hr>
                                 <div className="hidden-xs col-sm-6 project-img">
-                                    <img
-                                        className="img-responsive"
-                                        src={require("../images/movie_app_4.png")}
-                                        role={"presentation"} />
+                                    <a target="_blank" href="http://www.drewcoparker.com/AtlantaVibes">
+                                        <img
+                                            className="img-responsive"
+                                            src={require("../images/vibes2.png")}
+                                            role={"presentation"} />
+                                    </a>
                                 </div>
                                 <div className="hidden-sm hidden-md hidden-lg col-xs-12 project-img-sm">
                                     <img
                                         className="img-responsive"
-                                        src={require("../images/movie_app_4.png")}
+                                        src={require("../images/vibes2.png")}
                                         role={"presentation"} />
                                 </div>
                                 <div className="col-xs-12 col-sm-6 project-content-wrapper">
                                     <div className="project-content">
                                         <span>
                                             &nbsp;
-                                            Trailers is an app that lets movie lovers
-                                            browse now playing and upcoming movies
-                                            and quickly view their trailers. The
-                                            trailers are presented in a modal, so it's
-                                            quite easy to start one, dismiss it, and
-                                            move on to the next.
-                                        </span>
-                                        <br></br>
-                                        <br></br>
-                                        <span>
-                                            &nbsp;
-                                            Movies are displayed on handsome
-                                            cards featuring the poster, director,
-                                            runtime, and the mpaa rating. The
-                                            Movie Database provides the API.
+                                            AtlantaVibes is a full-stack application
+                                            that aggregates data from the top music
+                                            festivals, in Atlanta, into one easy to
+                                            use website. AtlantaVibes lets users view,
+                                            rate and comment on their favorite festivals
+                                            in Atlanta.
                                         </span>
                                         <hr></hr>
                                         <div id="tech-title"><span>Tech used</span></div>
                                         <div className="project-tech">
                                             <div className="tech-card"><span>React</span></div>
                                             <div className="tech-card"><span>Redux</span></div>
-                                            <div className="tech-card"><span>AJAX</span></div>
-                                            <div className="tech-card"><span>RESTful APIs</span></div>
+                                            <div className="tech-card"><span>Express.js</span></div>
+                                            <div className="tech-card"><span>Node.js</span></div>
+                                            <div className="tech-card"><span>MySql</span></div>
                                             <div className="tech-card"><span>jQuery</span></div>
                                             <div className="tech-card"><span>Bootstrap</span></div>
-                                            <div className="tech-card"><span>React-Router</span></div>
                                             <div className="tech-card"><span>ES6</span></div>
+                                            <div className="tech-card"><span>NPM</span></div>
+                                            <div className="tech-card"><span>Sass</span></div>
+                                            <div className="tech-card"><span>React-Router</span></div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                         <div className="row">
                             <div id="ebay" className="project-item">
                                 <div className="project-header">
@@ -139,13 +137,13 @@ class Projects extends Component {
                         </div>
 
                         <div className="row">
-                            <div id="geo" className="project-item">
+                            <div id="movie" className="project-item">
                                 <div className="project-header">
-                                    <div className="project-title"><span>AtlantaVibes</span>
-                                        <div className="project-sub-title">Get info on Atlanta's music festivals</div>
+                                    <div className="project-title"><span>Trailers</span>
+                                        <div className="project-sub-title">An app to view movie trailers</div>
                                     </div>
                                     <div className="github-repo">
-                                        <a target="_blank" href="https://github.com/drewcoparker/AtlantaVibes">
+                                        <a target="_blank" href="https://github.com/drewcoparker/movie-app-reactified">
                                             <div><span>GitHub</span></div>
                                         </a>
                                     </div>
@@ -154,45 +152,52 @@ class Projects extends Component {
                                 <div className="hidden-xs col-sm-6 project-img">
                                     <img
                                         className="img-responsive"
-                                        src={require("../images/vibes.png")}
+                                        src={require("../images/movie_app_4.png")}
                                         role={"presentation"} />
                                 </div>
                                 <div className="hidden-sm hidden-md hidden-lg col-xs-12 project-img-sm">
                                     <img
                                         className="img-responsive"
-                                        src={require("../images/vibes.png")}
+                                        src={require("../images/movie_app_4.png")}
                                         role={"presentation"} />
                                 </div>
                                 <div className="col-xs-12 col-sm-6 project-content-wrapper">
                                     <div className="project-content">
                                         <span>
                                             &nbsp;
-                                            AtlantaVibes is a full-stack application
-                                            that aggregates data from the top music
-                                            festivals, in Atlanta, into one easy to
-                                            use website. AtlantaVibes lets users view,
-                                            rate and comment on their favorite festivals
-                                            in Atlanta.
+                                            Trailers is an app that lets movie lovers
+                                            browse now playing and upcoming movies
+                                            and quickly view their trailers. The
+                                            trailers are presented in a modal, so it's
+                                            quite easy to start one, dismiss it, and
+                                            move on to the next.
+                                        </span>
+                                        <br></br>
+                                        <br></br>
+                                        <span>
+                                            &nbsp;
+                                            Movies are displayed on handsome
+                                            cards featuring the poster, director,
+                                            runtime, and the mpaa rating. The
+                                            Movie Database provides the API.
                                         </span>
                                         <hr></hr>
                                         <div id="tech-title"><span>Tech used</span></div>
                                         <div className="project-tech">
                                             <div className="tech-card"><span>React</span></div>
                                             <div className="tech-card"><span>Redux</span></div>
-                                            <div className="tech-card"><span>Express.js</span></div>
-                                            <div className="tech-card"><span>Node.js</span></div>
-                                            <div className="tech-card"><span>MySql</span></div>
+                                            <div className="tech-card"><span>AJAX</span></div>
+                                            <div className="tech-card"><span>RESTful APIs</span></div>
                                             <div className="tech-card"><span>jQuery</span></div>
                                             <div className="tech-card"><span>Bootstrap</span></div>
-                                            <div className="tech-card"><span>ES6</span></div>
-                                            <div className="tech-card"><span>NPM</span></div>
-                                            <div className="tech-card"><span>Sass</span></div>
                                             <div className="tech-card"><span>React-Router</span></div>
+                                            <div className="tech-card"><span>ES6</span></div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
